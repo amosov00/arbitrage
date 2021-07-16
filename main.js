33bc3cbf1a893ddec0e2d62c1819c9b1edd44b08
themashina <amosov00@gmail.com>
2021-07-16 16:29:41 +0300

    commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
-// (async () => {
-//     const P2PSchemeCalc = require('./p2pScheme/index.js')
-//     const {init} = require('./telegram')
-//     await init(P2PSchemeCalc)
-// })()
+(async () => {
+    console.log('привет пидорасы японские')
+    const P2PSchemeCalc = require('./p2pScheme/index.js')
+    const {init} = require('./telegram')
+    await init(P2PSchemeCalc)
+})()
 
-console.log('привет пидорасы японские')
