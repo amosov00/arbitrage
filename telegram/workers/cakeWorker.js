a795993179c8c5bcb014d2245e5f13c8c20bec34
Амосов Андрей <aamosov@rzdit.ru>
2021-09-14 10:23:51 +0300

    commit

--- telegram/workers/cakeWorker.js
+++ telegram/workers/cakeWorker.js
@@ -3,7 +3,9 @@ const {cakeCalc} = require('../../cakeSheme/index.js')
 
 async function repeat() {
     await new Promise((resolve)=>{setTimeout(()=>{resolve()}, 1200)})
+    console.time('скорость обновления')
     const calcResponse = await cakeCalc(parseInt(workerData.amountIn))
+    console.timeEnd('скорость обновления')
     const procent = ((calcResponse * 100) / workerData.amountIn) - 100
     console.log('-'.repeat(50))
     console.log('Cхема с питупи гарой и панкейком(BNB)')
